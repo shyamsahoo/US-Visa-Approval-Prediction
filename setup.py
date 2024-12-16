77d793c397474b1e4eb36d58a702f67fe8be7133
Shyam Sundar Sahoo <shyamsundarsahoo2002@gmail.com>
2024-12-16 08:48:33 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.0",
+    author="Shyam Sundar Sahoo",
+    author_email="shyamsundarsahoo2002@gmail.com",
+    packages=find_packages()
+)
